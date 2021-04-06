a8a141ab8ba5cdd8a3b6a73dbf1b4328fa2c7ca2
q2315648899 <406972433@qq.com>
2021-04-06 20:49:33 +0800

    json字符串转化成对象时会自动识别java对象的驼峰属性； 对象转化成json字符串时，若json字符串参数是类似client_id形式，java对象属性想用clientId驼峰命名的话，在java属性上添加@JSONField(name = "XXX")。

--- src/main/java/life/majiang/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -7,5 +7,5 @@ public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-    private String avatar_url;
+    private String avatar_url;// json字符串中命名为avatar_url，此处属性也可写成avatarUrl驼峰命名，fastjson把json字符串转换成对象时会自动识别对象中的驼峰命名属性
 }
