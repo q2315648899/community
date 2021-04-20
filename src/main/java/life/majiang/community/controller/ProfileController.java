f87fd548f36e9c3d18c1ac4836394d6d73ad8bae
q2315648899 <406972433@qq.com>
2021-04-20 22:40:03 +0800

    增加富文本编辑

--- src/main/java/life/majiang/community/controller/ProfileController.java
+++ src/main/java/life/majiang/community/controller/ProfileController.java
@@ -44,8 +44,6 @@ public class ProfileController {
             model.addAttribute("sectionName", "最新回复");
 
             PaginationDTO paginationDTO = notificationService.list(user.getId(), page, size);
-            Long unreadCount = notificationService.unreadCount(user.getId());
-            model.addAttribute("unreadCount", unreadCount);
             model.addAttribute("pagination", paginationDTO);
         }
 
