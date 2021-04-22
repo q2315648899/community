29ed54fc35c0f9e24b407768ddefd33ab2e0e262
q2315648899 <406972433@qq.com>
2021-04-22 22:16:20 +0800

    使用SDK上传图片

--- src/main/java/life/majiang/community/controller/FileController.java
+++ src/main/java/life/majiang/community/controller/FileController.java
@@ -1,16 +1,34 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.FileDTO;
+import life.majiang.community.provider.UcloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileController {
 
+    @Autowired
+    private UcloudProvider ucloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload() {
+    public FileDTO upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
+        try {
+            String upload = ucloudProvider.upload(file.getInputStream(), file
+                    .getContentType(), file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/WeChat.png");
