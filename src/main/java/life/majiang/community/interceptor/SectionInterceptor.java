67cdc88d62e7a02af16962ae4b5c40230c8eaea2
q2315648899 <406972433@qq.com>
2021-04-26 20:53:41 +0800

    修复登录问题2

--- src/main/java/life/majiang/community/interceptor/SectionInterceptor.java
+++ src/main/java/life/majiang/community/interceptor/SectionInterceptor.java
@@ -5,6 +5,7 @@ import life.majiang.community.model.User;
 import life.majiang.community.model.UserExample;
 import life.majiang.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,8 +24,13 @@ public class SectionInterceptor implements HandlerInterceptor {
     @Autowired
     private NotificationService notificationService;
 
+//    @Value("${github.redirect.uri}")
+//    private String redirectUri;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //设置 context 级别的属性
+//        request.getServletContext().setAttribute("redirectUri", redirectUri);
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
