e688ba8cda5067744798e7f9348a4adc27fce79d
q2315648899 <406972433@qq.com>
2021-04-26 22:50:48 +0800

    修改数据库脚本

--- src/main/resources/db/migration/V7__Change_id_column_type.sql
+++ src/main/resources/db/migration/V7__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table question modify id bigint NOT NULL;
-alter table `user` modify id bigint NOT NULL;
+alter table question modify id bigint not null;
+alter table `user` modify id bigint not null;
