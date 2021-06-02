1444745a85bb5f397b96c557a942fea60e5a8120
q2315648899 <406972433@qq.com>
2021-06-02 17:53:34 +0800

    添加代码注释

--- src/main/java/life/majiang/community/dto/AccessTokenDTO.java
+++ src/main/java/life/majiang/community/dto/AccessTokenDTO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class AccessTokenDTO {
-    // 对象属性命名是啥，json转化成的字符串参数命名就是啥
-    // 这些都是post的json参数固定的命名，若不使用这个命名使用驼峰命名clientId，
+    // 对象属性命名是啥，转化成的json字符串参数命名就是啥
+    // 这些都是post请求体中带的json参数固定的命名，若不使用这个命名使用驼峰命名clientId，
     // 构造成json字符串时也会使用clientId参数名称和client_id不一致导致报错。
     // 若想使用驼峰命名，在指定属性上添加@JSONField(name = "XXX")注解，XXX是json中参数命名，下面的属性就可以用clientId驼峰命名了
     private String client_id;
